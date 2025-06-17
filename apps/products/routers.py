0fae69b74ab403d9e17bbae8b5269d2ffd2989e2
Алексей <aleksejgerman06@gmail.com>
2025-06-17 12:40:46 +0300

    14.2 - добавил что количество просмотренных ограничено (10)

--- apps/products/routers.py
+++ apps/products/routers.py
@@ -1,4 +1,3 @@
-
 from fastapi import APIRouter, HTTPException, Query, Depends
 
 from apps.products.models import NewProduct, AddProdBask
@@ -45,44 +44,40 @@ async def add_product(new_Product: NewProduct):
         )
 
 
-
 @router.get("/all-product", summary="получить все продукты")
 async def get_all_products(sort: int, id_profile: int | None = Query(default=0)):
-
     products = await ProductService.select_all_product(sort, id_profile)
-
 
     return products
 
 
 @router.get("/filter-product", summary="фильтрация продуктов")
-async def get_all_products(page: int = Query(ge=0,default=0),
-                           size: int = Query(ge=1,le=100),
-                           brand:str | None = Query(default=None, min_length=2, max_length=25),                        
-                           price_filtr:str | None = Query(default=None, min_length=3, max_length=4, pattern="^(asc|desc)$",  description="Сортировка цены"),
-                           popular:str | None = Query(default=None, max_length=4, pattern="^(true)$",description="Сортировка по популярности"),
-                           min_price:int | None = Query(default=None,  description="минимальная цена"),
-                           max_price:int | None = Query(default=None,  description="максимальная цена"),
+async def get_all_products(page: int = Query(ge=0, default=0),
+                           size: int = Query(ge=1, le=100),
+                           brand: str | None = Query(default=None, min_length=2, max_length=25),
+                           price_filtr: str | None = Query(default=None, min_length=3, max_length=4,
+                                                           pattern="^(asc|desc)$", description="Сортировка цены"),
+                           popular: str | None = Query(default=None, max_length=4, pattern="^(true)$",
+                                                       description="Сортировка по популярности"),
+                           min_price: int | None = Query(default=None, description="минимальная цена"),
+                           max_price: int | None = Query(default=None, description="максимальная цена"),
 
                            id_profile: int | None = Query(default=0)
                            ):
-
-
-    products = await ProductService.filter_product(brand,price_filtr,popular,min_price,max_price,page,size,id_profile)
+    products = await ProductService.filter_product(brand, price_filtr, popular, min_price, max_price, page, size,
+                                                   id_profile)
 
     return products
 
 
 @router.get("/one-product/{id_product}", summary="получить один продукт")
 async def get_one_products(id_product: int):
-
     products = await ProductService.one_product(id_product)
     return products
 
 
 @router.delete("/product/{id_product}", summary="удалить продукт")
 async def del_product(id_product: int):
-
     product = await ProductService.del_product(id_product)
 
     if product is not None:
@@ -90,8 +85,7 @@ async def del_product(id_product: int):
 
 
 @router_basket.post("/basket-prod", summary="добавить продукт в корзину")
-async def add_prod_bask( add_prod_bask : AddProdBask):
-   
+async def add_prod_bask(add_prod_bask: AddProdBask):
     try:
 
         prod_bask_id = await ProductService.add_product_bask(add_prod_bask)
@@ -108,10 +102,10 @@ async def add_prod_bask( add_prod_bask : AddProdBask):
             status_code=500,
             detail=f"Ошибка при добавлении продукта в корзину: {str(e)}",
         )
-    
+
+
 @router_basket.get("/all-product-bask/{id_user}", summary="получить все продукты")
 async def get_all_products(id_user: int):
-
     products_bask = await ProductService.sellect_product_bask(id_user)
 
     return products_bask
@@ -134,23 +128,22 @@ async def del_prod_bask(id_us_storage: int):
     except Exception as e:
 
         raise HTTPException(
-            status_code=500, 
+            status_code=500,
             detail=f"Ошибка при удалении номенкулатуры: {str(e)}"
         )
 
 
 @router_brand.get("/all-brand", summary="получить все бренды")
 async def get_all_brand():
-   
-
-    brands=await ProductService.select_brands()
+    brands = await ProductService.select_brands()
     return brands
+
 
 @router_order.post("/checkout", summary="Оформить заказ")
 async def checkout_order(
-    order_data: CheckoutOrderRequest,
-    current_user: Profile = Depends(get_current_user),
-    db: AsyncSession = Depends(get_async_db)
+        order_data: CheckoutOrderRequest,
+        current_user: Profile = Depends(get_current_user),
+        db: AsyncSession = Depends(get_async_db)
 ):
     try:
         order_proc_id = await OrderService.create_order_with_processor(order_data, current_user.id_profile, db)
@@ -161,3 +154,9 @@ async def checkout_order(
         }
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Ошибка при оформлении заказа: {str(e)}")
+
+
+@router.get("/serch", summary="поиск категорий и продуктов")
+async def serch(litters: str):
+    res = await ProductService.serch(litters)
+    return res

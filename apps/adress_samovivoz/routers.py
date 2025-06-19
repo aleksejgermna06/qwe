c571bd33f7ed64f5e0c6976e7b2ad5aada8aab74
Алексей <aleksejgerman06@gmail.com>
2025-06-19 11:56:46 +0300

    14.3 - фикса самовывоза

--- apps/adress_samovivoz/routers.py
+++ apps/adress_samovivoz/routers.py
@@ -29,6 +29,28 @@ async def delete_adress(adress_id: int, session: AsyncSession = Depends(get_asyn
 
 @router.get("/", response_model=List[AdressOut])
 async def get_all(session: AsyncSession = Depends(get_async_session)):
+    default_addresses = [
+        "г. Москва, ул. Ленина, 10",
+        "г. Санкт-Петербург, пр. Невский, 25",
+        "г. Екатеринбург, ул. Мира, 5",
+        "г. Казань, ул. Баумана, 12",
+        "г. Новосибирск, пр. Красный, 42",
+        "г. Киев, ул. Крещатик, 15",
+        "г. Минск, пр. Независимости, 32",
+        "г. Алматы, ул. Абая, 56",
+        "г. Тбилиси, пр. Руставели, 14",
+        "г. Берлин, Unter den Linden, 77",
+        "г. Париж, Rue de Rivoli, 23",
+        "г. Токио, район Сибуя, 1-2-3",
+        "г. Нью-Йорк, Broadway, 100",
+        "г. Сидней, George Street, 42",
+        "г. Затупеево, ул. Пупкина, 13"
+    ]
+    for addr in default_addresses:
+        exists_query = await session.execute(select(AdressSamovivoz).where(AdressSamovivoz.adress == addr))
+        if not exists_query.scalar():
+            session.add(AdressSamovivoz(adress=addr))
+    await session.commit()
     result = await session.execute(select(AdressSamovivoz))
     return result.scalars().all()
 

6e24b46264f13431bea09dd7f2eb8d554c007c37
vladpyth <67658799+vladpyth@users.noreply.github.com>
2025-06-19 20:52:27 +0300

    Update service.py

--- apps/products/service.py
+++ apps/products/service.py
@@ -376,9 +376,9 @@ class ProductService:
                                         UserAction.profile_id == id_profile
                                     )
                                 ),
-                                "true"
+                                True
                             ),
-                            else_="false"
+                            else_=False
                         ).label("in_fav"),
                         case(
                             (
@@ -388,9 +388,9 @@ class ProductService:
                                         UserBasket.id_profile == id_profile
                                     )
                                 ),
-                                "true"
+                                True
                             ),
-                            else_="false"
+                            else_=False
                         ).label("in_cart"),
                         Entity,
                         Gfields,

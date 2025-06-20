cfc5b9ba9255c399645749233937be5eabf0d0f3
Алексей <aleksejgerman06@gmail.com>
2025-06-20 10:07:43 +0300

    14.3 - фикса Влада

--- apps/products/service.py
+++ apps/products/service.py
@@ -15,8 +15,8 @@ from fastapi import HTTPException
 from sqlalchemy import delete, func, join, select, cast, Numeric, nulls_last, case, and_, exists,update
 
 from core.database import get_async_db
-from core.models import Action, Categories, Product, Reviews, metadata_obj, UserBasket, Entity, Gfields, Categories, UserAction
-
+from core.models import Action, Categories, Product, Reviews, metadata_obj, UserBasket, Entity, Gfields, Categories
+from apps.user_actions.models import UserAction
 session_fabrik = get_async_db
 
 
